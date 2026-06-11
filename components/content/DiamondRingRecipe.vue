04693bac0abc937481b69201dc7024415152dd0b
TN145376 <terranovaworld1@gmail.com>
2026-06-11 02:53:39 +0100

    Add files via upload

---
+++ components/content/DiamondRingRecipe.vue
@@ -0,0 +1,23 @@
+<template>
+  <CraftingRecipe
+    title="Diamond Ring"
+    :grid="grid"
+    :output="output"
+  />
+</template>
+
+<script setup>
+const diamond = { name: 'Diamond', image: '/images/items/diamond.png' }
+
+const grid = [
+  diamond, diamond, diamond,
+  diamond, null,    diamond,
+  diamond, diamond, diamond,
+]
+
+const output = {
+  name: 'Your Custom Item',
+  image: '/images/items/your-item.png',
+  count: 1,
+}
+</script>
