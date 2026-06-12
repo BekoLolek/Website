f5fe27bdcee0b1ba6784d003fd59745ee00f0392
TN145376 <terranovaworld1@gmail.com>
2026-06-12 02:17:22 +0100

    Create Level2.vue

---
+++ components/content/Level2.vue
@@ -0,0 +1,25 @@
+<template>
+  <CraftingRecipe
+    title="Level 2 Template"
+    :grid="grid"
+    :output="output"
+  />
+</template>
+
+<script setup>
+const diamond = { name: 'Diamond', image: '/images/items/diamond.png', count: 32 }
+const n = { name: 'Ender Eye', image: '/images/items/n.png', }
+const 1 = { name: 'Ender Eye', image: '/images/items/1.png', }
+
+const grid = [
+  n, 1, n,
+  1, diamond,    1,
+  n, 1, n,
+]
+
+const output = {
+  name: 'Your Custom Item',
+  image: '/images/items/2.png',
+  count: 1,
+}
+</script>
