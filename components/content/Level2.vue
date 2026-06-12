901fd0f457b8b9b3194c247c464c3b46a17c8979
TN145376 <terranovaworld1@gmail.com>
2026-06-12 02:27:42 +0100

    Update Level2.vue

--- components/content/Level2.vue
+++ components/content/Level2.vue
@@ -9,12 +9,12 @@
 <script setup>
 const diamond = { name: 'Diamond', image: '/images/items/diamond.png', count: 32 }
 const p = { name: 'p', image: '/images/items/p.png', }
-const 1 = { name: '1', image: '/images/items/1.png', }
+const num1 = { name: '1', image: '/images/items/1.png', }
 
 const grid = [
-  p, 1, p,
-  1, diamond, 1,
-  p, 1, p,
+  p, num1, p,
+  num1, diamond, num1,
+  p, num1, p,
 ]
 
 const output = {
