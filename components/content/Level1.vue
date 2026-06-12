3296164b94fdbbf0cab99219c24b8e44a7b28082
TN145376 <terranovaworld1@gmail.com>
2026-06-12 01:56:30 +0100

    Update and rename DiamondRingRecipe.vue to Level1.vue

---
+++ components/content/Level1.vue
@@ -0,0 +1,24 @@
+<template>
+  <CraftingRecipe
+    title="Level 1 Template"
+    :grid="grid"
+    :output="output"
+  />
+</template>
+
+<script setup>
+const diamond = { name: 'Diamond', image: '/images/items/diamond.png', count: 16 }
+const eye = { name: 'Ender Eye', image: '/images/items/eye.png', }
+
+const grid = [
+  diamond, null, diamond,
+  null, eye,    null,
+  diamond, null, diamond,
+]
+
+const output = {
+  name: 'Your Custom Item',
+  image: '/images/items/your-item.png',
+  count: 1,
+}
+</script>
